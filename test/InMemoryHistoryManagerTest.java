4cabc327c4c02934a19430343bd3a7a51b4e6b17
Aleksandr Onishchuk <aleksandr200689@yandex.ru>
2025-05-26 16:50:32 +0800

    Спринт 6

--- test/InMemoryHistoryManagerTest.java
+++ test/InMemoryHistoryManagerTest.java
@@ -15,4 +15,31 @@ class InMemoryHistoryManagerTest {
         Task taskFromHistory = historyManager.getHistory().get(0);;
         assertEquals("Задача 1", taskFromHistory.getTitle());
     }
+
+    @Test
+    public void addTaskTest() {
+        InMemoryHistoryManager historyManager = new InMemoryHistoryManager();
+        Task task = new Task(1, "Задача 1", "Описание 1");
+        historyManager.add(task);
+        assertEquals(1, historyManager.getHistory().size());
+    }
+
+    @Test
+    public void removeTaskTest() {
+        InMemoryHistoryManager historyManager = new InMemoryHistoryManager();
+        Task task = new Task(1, "Задача 1", "Описание 1");
+        historyManager.add(task);
+        historyManager.remove(1);
+        assertEquals(0, historyManager.getHistory().size());
+    }
+
+    @Test
+    public void getHistoryTest() {
+        InMemoryHistoryManager historyManager = new InMemoryHistoryManager();
+        Task task1 = new Task(1, "Задача 1", "Описание 1");
+        Task task2 = new Task(2, "Задача 2", "Описание 2");
+        historyManager.add(task1);
+        historyManager.add(task2);
+        assertEquals(2, historyManager.getHistory().size());
+    }
 }
