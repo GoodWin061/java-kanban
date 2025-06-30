c263469c1cb837bf1f159c1252b8a1316fb99d15
Aleksandr Onishchuk <aleksandr200689@yandex.ru>
2025-07-01 00:40:13 +0800

    Спринт 8

--- src/main/task/Task.java
+++ src/main/task/Task.java
@@ -61,13 +61,21 @@ public class Task {
         this.status = status;
     }
 
-    public Duration getDuration() { return duration; }
+    public Duration getDuration() {
+        return duration;
+    }
 
-    public void setDuration(Duration duration) { this.duration = duration; }
+    public void setDuration(Duration duration) {
+        this.duration = duration;
+    }
 
-    public LocalDateTime getStartTime() { return startTime; }
+    public LocalDateTime getStartTime() {
+        return startTime;
+    }
 
-    public void setStartTime(LocalDateTime startTime) { this.startTime = startTime; }
+    public void setStartTime(LocalDateTime startTime) {
+        this.startTime = startTime;
+    }
 
     public LocalDateTime getEndTime() {
         return startTime.plus(duration);
